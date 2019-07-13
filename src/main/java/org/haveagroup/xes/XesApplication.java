220e2a2e0f3ff8bd0d998c820d2bdaca77c5fcc0
knifec <ckxgoodzc@gmail.com>
2019-07-13 13:40:06 +0800

    init commit

---
+++ src/main/java/org/haveagroup/xes/XesApplication.java
@@ -0,0 +1,13 @@
+package org.haveagroup.xes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/haveagroup/xes/XesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.haveagroup.xes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
