eb02774d1c6bef1bfdddb18a64440c238c604feb
knifec <ckxgoodzc@gmail.com>
2019-07-15 09:56:45 +0800

    debug null string pointer

--- src/main/java/org/haveagroup/xes/Util/StringUtil.java
+++ src/main/java/org/haveagroup/xes/Util/StringUtil.java
@@ -2,6 +2,9 @@ package org.haveagroup.xes.Util;
 
 public class StringUtil {
     public static boolean isEquals(String str1,String str2){
+        if(str1==null||str2==null){
+            return false;
+        }
         if(str1.equals(str2)){
             return true;
         }else{
