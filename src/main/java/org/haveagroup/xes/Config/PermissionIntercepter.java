a91119ae9c4f1c546ddde4ab9c2daf1aee0b2d73
knifec <ckxgoodzc@gmail.com>
2019-07-15 10:35:10 +0800

    perminssionintercepter 增加logger

--- src/main/java/org/haveagroup/xes/Config/PermissionIntercepter.java
+++ src/main/java/org/haveagroup/xes/Config/PermissionIntercepter.java
@@ -1,6 +1,8 @@
 package org.haveagroup.xes.Config;
 
 import org.haveagroup.xes.Commom.SessionKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,8 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class PermissionIntercepter implements HandlerInterceptor {
+
+    Logger logger = LoggerFactory.getLogger(PermissionIntercepter.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("进入PermissionIntercepter");
         HttpSession session = request.getSession();
         //登录验证
         Object usernameObj = session.getAttribute(SessionKey.USER_NAME);
